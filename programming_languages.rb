25cb0449f417686310b43164f2d024d55026f14e
Sam Markham <sam.e.markham@gmail.com>
2019-06-06 18:54:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,9 +4,9 @@ def reformat_languages(languages)
   new_language_hash = {}
   languages.each do |type, language|
     if type == :oo 
+      binding.pry
       language[:style] = [:oo]
       new_language_hash << language 
-      binding.pry
     end 
   end 
 end
